8d5a1f6f13441156202edf5cf5812f2a3fc19339
Nicolas Frankel <nicolas@frankel.ch>
2013-07-24 17:03:39 +0200

    Continued componentization with extraction of reusable UI component

---
+++ src/main/java/ch/frankel/duchessswiss/vaadin/ui/LoginScreen.java
@@ -0,0 +1,42 @@
+package ch.frankel.duchessswiss.vaadin.ui;
+
+import ch.frankel.duchessswiss.vaadin.behavior.DummyListener;
+import com.vaadin.ui.*;
+
+import static com.vaadin.server.Sizeable.Unit.PERCENTAGE;
+import static com.vaadin.server.Sizeable.Unit.PIXELS;
+import static com.vaadin.ui.Alignment.MIDDLE_CENTER;
+
+public class LoginScreen extends VerticalLayout {
+
+    LoginScreen() {
+
+        setSizeFull();
+        setMargin(true);
+
+        Panel panel = new Panel();
+        panel.setHeight(200, PIXELS);
+        panel.setWidth(300, PIXELS);
+
+        addComponent(panel);
+        setComponentAlignment(panel, MIDDLE_CENTER);
+
+        final FormLayout panelLayout = new FormLayout();
+        panelLayout.setMargin(true);
+        panelLayout.setSpacing(true);
+        panelLayout.setSizeFull();
+
+        panel.setContent(panelLayout);
+
+        final TextField loginField = new TextField("Username");
+        loginField.setWidth(100, PERCENTAGE);
+
+        panelLayout.addComponent(loginField);
+
+        Button button = new Button("Login");
+
+        button.addClickListener(new DummyListener(panelLayout, loginField));
+
+        panelLayout.addComponent(button);
+    }
+}
--- src/main/java/ch/frankel/duchessswiss/vaadin/ui/MyVaadinUI.java
+++ src/main/java/ch/frankel/duchessswiss/vaadin/ui/MyVaadinUI.java
@@ -1,17 +1,12 @@
 package ch.frankel.duchessswiss.vaadin.ui;
 
-import javax.servlet.annotation.WebServlet;
-
-import ch.frankel.duchessswiss.vaadin.behavior.DummyListener;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.*;
+import com.vaadin.ui.UI;
 
-import static com.vaadin.server.Sizeable.Unit.PERCENTAGE;
-import static com.vaadin.server.Sizeable.Unit.PIXELS;
-import static com.vaadin.ui.Alignment.MIDDLE_CENTER;
+import javax.servlet.annotation.WebServlet;
 
 @SuppressWarnings("serial")
 @Title("Duchess Vaadin demo")
@@ -25,35 +20,6 @@ public class MyVaadinUI extends UI {
     @Override
     protected void init(VaadinRequest request) {
 
-        final VerticalLayout mainLayout = new VerticalLayout();
-        mainLayout.setSizeFull();
-        mainLayout.setMargin(true);
-
-        setContent(mainLayout);
-
-        Panel panel = new Panel();
-        panel.setHeight(200, PIXELS);
-        panel.setWidth(300, PIXELS);
-
-        mainLayout.addComponent(panel);
-        mainLayout.setComponentAlignment(panel, MIDDLE_CENTER);
-
-        final FormLayout panelLayout = new FormLayout();
-        panelLayout.setMargin(true);
-        panelLayout.setSpacing(true);
-        panelLayout.setSizeFull();
-
-        panel.setContent(panelLayout);
-
-        final TextField loginField = new TextField("Username");
-        loginField.setWidth(100, PERCENTAGE);
-
-        panelLayout.addComponent(loginField);
-
-        Button button = new Button("Login");
-
-        button.addClickListener(new DummyListener(panelLayout, loginField));
-
-        panelLayout.addComponent(button);
+        setContent(new LoginScreen());
     }
 }
