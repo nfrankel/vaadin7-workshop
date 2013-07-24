2c066adc36fc3eb098333abaf53c616f47499630
Nicolas Frankel <nicolas@frankel.ch>
2013-07-24 15:10:27 +0200

    Cleaned up additional files brought by archetype
      Removed theme
      Removed widgetset

--- src/main/java/ch/frankel/duchessswiss/vaadin/MyVaadinUI.java
+++ src/main/java/ch/frankel/duchessswiss/vaadin/MyVaadinUI.java
@@ -12,29 +12,34 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-@Theme("mytheme")
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI
 {
 
     @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "ch.frankel.duchessswiss.vaadin.AppWidgetSet")
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class)
     public static class Servlet extends VaadinServlet {
     }
 
     @Override
     protected void init(VaadinRequest request) {
+
         final VerticalLayout layout = new VerticalLayout();
+
         layout.setMargin(true);
+
         setContent(layout);
         
         Button button = new Button("Click Me");
+
         button.addClickListener(new Button.ClickListener() {
+
             public void buttonClick(ClickEvent event) {
+
                 layout.addComponent(new Label("Thank you for clicking"));
             }
         });
+
         layout.addComponent(button);
     }
-
 }
