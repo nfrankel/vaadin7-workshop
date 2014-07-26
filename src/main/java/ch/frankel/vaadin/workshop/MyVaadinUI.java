cd6e7e9a10683c74ab896a65f147352dd540af7d
Nicolas Frankel <nicolas@frankel.ch>
2014-07-26 19:01:38 +0200

    Extract the servlet configuration into a dedicated class

--- src/main/java/ch/frankel/vaadin/workshop/MyVaadinUI.java
+++ src/main/java/ch/frankel/vaadin/workshop/MyVaadinUI.java
@@ -1,22 +1,13 @@
 package ch.frankel.vaadin.workshop;
 
-import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.*;
-
-import javax.servlet.annotation.WebServlet;
 
 import static com.vaadin.server.Sizeable.Unit.PIXELS;
 
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI {
-
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class)
-    public static class Servlet extends VaadinServlet {
-    }
 
     @Override
     protected void init(VaadinRequest request) {
---
+++ src/main/java/ch/frankel/vaadin/workshop/web/WorkshopServlet.java
@@ -0,0 +1,11 @@
+package ch.frankel.vaadin.workshop.web;
+
+import ch.frankel.vaadin.workshop.MyVaadinUI;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(value = "/*", asyncSupported = true)
+@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class)
+public class WorkshopServlet extends VaadinServlet {}
