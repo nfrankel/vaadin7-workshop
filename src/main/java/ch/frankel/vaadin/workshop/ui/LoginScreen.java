09df6d773e49a9f8183b62a8f92ba1974ce413f4
Nicolas Frankel <nicolas@frankel.ch>
2014-07-26 21:45:02 +0200

    Extract UI component into its own class

---
+++ src/main/java/ch/frankel/vaadin/workshop/ui/LoginScreen.java
@@ -0,0 +1,23 @@
+package ch.frankel.vaadin.workshop.ui;
+
+import ch.frankel.vaadin.workshop.behavior.DummyClickListener;
+import com.vaadin.server.Page;
+import com.vaadin.ui.*;
+
+import static com.vaadin.server.Sizeable.Unit.PIXELS;
+
+public class LoginScreen extends FormLayout {
+
+    public LoginScreen() {
+        setMargin(true);
+        Button button = new Button("Click Me");
+        button.addClickListener(new DummyClickListener(this));
+        addComponent(new Label("Please enter credentials"));
+        TextField login = new TextField("Login:");
+        login.setWidth(250, PIXELS);
+        addComponent(login);
+        addComponent(new PasswordField("Password:"));
+        addComponent(button);
+        Page.getCurrent().setTitle("Vaadin workshop (" + System.currentTimeMillis() + ")");
+    }
+}
--- src/main/java/ch/frankel/vaadin/workshop/ui/MyVaadinUI.java
+++ src/main/java/ch/frankel/vaadin/workshop/ui/MyVaadinUI.java
@@ -1,28 +1,13 @@
 package ch.frankel.vaadin.workshop.ui;
 
-import com.vaadin.server.Page;
-import ch.frankel.vaadin.workshop.behavior.DummyClickListener;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.*;
-
-import static com.vaadin.server.Sizeable.Unit.PIXELS;
+import com.vaadin.ui.UI;
 
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        final AbstractOrderedLayout layout = new FormLayout();
-        layout.setMargin(true);
-        setContent(layout);
-        Button button = new Button("Click Me");
-        button.addClickListener(new DummyClickListener(layout));
-        layout.addComponent(new Label("Please enter credentials"));
-        TextField login = new TextField("Login:");
-        login.setWidth(250, PIXELS);
-        layout.addComponent(login);
-        layout.addComponent(new PasswordField("Password:"));
-        layout.addComponent(button);
-        Page.getCurrent().setTitle("Vaadin workshop (" + System.currentTimeMillis() + ")");
+        setContent(new LoginScreen());
     }
 }
