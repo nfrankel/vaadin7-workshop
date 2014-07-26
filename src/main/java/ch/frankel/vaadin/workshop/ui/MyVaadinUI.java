3de7d2a3425bef98c5549e1b75b8ea798c47b3c3
Nicolas Frankel <nicolas@frankel.ch>
2014-07-26 21:45:02 +0200

    Extracted UI component into its own class

--- src/main/java/ch/frankel/vaadin/workshop/ui/MyVaadinUI.java
+++ src/main/java/ch/frankel/vaadin/workshop/ui/MyVaadinUI.java
@@ -1,11 +1,8 @@
 package ch.frankel.vaadin.workshop.ui;
 
-import ch.frankel.vaadin.workshop.behavior.DummyClickListener;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.*;
-
-import static com.vaadin.server.Sizeable.Unit.PIXELS;
+import com.vaadin.ui.UI;
 
 @SuppressWarnings("serial")
 @Title("Vaadin Workshop")
@@ -13,16 +10,6 @@ public class MyVaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        final AbstractOrderedLayout layout = new FormLayout();
-        layout.setMargin(true);
-        setContent(layout);
-        Button button = new Button("Click Me");
-        button.addClickListener(new DummyClickListener(layout));
-        layout.addComponent(new Label("Please enter credentials"));
-        TextField login = new TextField("Login:");
-        login.setWidth(250, PIXELS);
-        layout.addComponent(login);
-        layout.addComponent(new PasswordField("Password:"));
-        layout.addComponent(button);
+        setContent(new LoginScreen());
     }
 }
