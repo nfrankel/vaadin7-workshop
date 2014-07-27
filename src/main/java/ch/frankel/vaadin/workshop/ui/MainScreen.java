8ac2ede6f16c74879666983fab588edf712afc93
Nicolas Frankel <nicolas@frankel.ch>
2014-07-27 19:34:52 +0200

    Update the main screen with needed components

--- src/main/java/ch/frankel/vaadin/workshop/ui/MainScreen.java
+++ src/main/java/ch/frankel/vaadin/workshop/ui/MainScreen.java
@@ -1,12 +1,27 @@
 package ch.frankel.vaadin.workshop.ui;
 
 import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 
-public class MainScreen extends VerticalLayout {
+import static com.vaadin.server.Sizeable.Unit.PERCENTAGE;
+
+public class MainScreen extends CustomComponent {
 
     public MainScreen() {
-        super(new Label(VaadinSession.getCurrent().getAttribute(String.class)));
+        Label loginLabel = new Label("Welcome " + VaadinSession.getCurrent().getAttribute(String.class));
+        HorizontalLayout menuBar = new HorizontalLayout(loginLabel);
+        Grid grid = new Grid();
+        grid.setSizeFull();
+        TextArea messageArea = new TextArea();
+        messageArea.setWidth(100, PERCENTAGE);
+        Button sendButton = new Button("Send");
+        HorizontalLayout lowerBar = new HorizontalLayout(messageArea, sendButton);
+        lowerBar.setWidth(100, PERCENTAGE);
+        lowerBar.setSpacing(true);
+        VerticalLayout mainLayout = new VerticalLayout(menuBar, grid, lowerBar);
+        mainLayout.setSpacing(true);
+        mainLayout.setMargin(true);
+        mainLayout.setSizeFull();
+        setCompositionRoot(mainLayout);
     }
 }
