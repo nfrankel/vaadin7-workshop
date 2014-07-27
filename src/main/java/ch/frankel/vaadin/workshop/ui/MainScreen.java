b91580d3a296263f9a39a23200fcb2bae1b2d650
Nicolas Frankel <nicolas@frankel.ch>
2014-07-27 11:12:44 +0200

    Added a new MainScreen component

---
+++ src/main/java/ch/frankel/vaadin/workshop/ui/MainScreen.java
@@ -0,0 +1,11 @@
+package ch.frankel.vaadin.workshop.ui;
+
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Label;
+
+public class MainScreen extends CustomComponent {
+
+    public MainScreen() {
+        super(new Label("Main"));
+    }
+}
