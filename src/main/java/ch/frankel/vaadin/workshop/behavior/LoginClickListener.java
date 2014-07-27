55186c36ed61ea74fac4425874eead82645d1450
Nicolas Frankel <nicolas@frankel.ch>
2014-07-27 13:56:20 +0200

    Add a simple notification after authentication

--- src/main/java/ch/frankel/vaadin/workshop/behavior/LoginClickListener.java
+++ src/main/java/ch/frankel/vaadin/workshop/behavior/LoginClickListener.java
@@ -2,6 +2,7 @@ package ch.frankel.vaadin.workshop.behavior;
 
 import ch.frankel.vaadin.workshop.ui.MainScreen;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 
 public class LoginClickListener implements Button.ClickListener {
@@ -9,5 +10,6 @@ public class LoginClickListener implements Button.ClickListener {
     @Override
     public void buttonClick(Button.ClickEvent event) {
         UI.getCurrent().setContent(new MainScreen());
+        Notification.show("You've been successfully logged in");
     }
 }
