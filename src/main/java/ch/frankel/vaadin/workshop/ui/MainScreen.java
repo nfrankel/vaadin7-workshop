720f62813a41b66d7947b4f4d956813aebfd35b3
Nicolas Frankel <nicolas@frankel.ch>
2014-07-27 16:19:03 +0200

    Store the user name at login and display it on the main screen

--- src/main/java/ch/frankel/vaadin/workshop/ui/MainScreen.java
+++ src/main/java/ch/frankel/vaadin/workshop/ui/MainScreen.java
@@ -1,11 +1,12 @@
 package ch.frankel.vaadin.workshop.ui;
 
+import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
 
 public class MainScreen extends CustomComponent {
 
     public MainScreen() {
-        super(new Label("Main"));
+        super(new Label(VaadinSession.getCurrent().getAttribute(String.class)));
     }
 }
