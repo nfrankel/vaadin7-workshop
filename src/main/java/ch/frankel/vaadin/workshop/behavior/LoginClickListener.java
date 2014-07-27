ea577f669d44120d07085c8d3c7c2907a7eb3f58
Nicolas Frankel <nicolas@frankel.ch>
2014-07-27 14:52:42 +0200

    Implemented a simple authentication logic and the relevant error notification

--- src/main/java/ch/frankel/vaadin/workshop/behavior/LoginClickListener.java
+++ src/main/java/ch/frankel/vaadin/workshop/behavior/LoginClickListener.java
@@ -1,15 +1,27 @@
 package ch.frankel.vaadin.workshop.behavior;
 
 import ch.frankel.vaadin.workshop.ui.MainScreen;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+
+import static com.vaadin.ui.Notification.Type.ERROR_MESSAGE;
 
 public class LoginClickListener implements Button.ClickListener {
 
+    private TextField loginField;
+    private PasswordField passwordField;
+
+    public LoginClickListener(TextField loginField, PasswordField passwordField) {
+        this.loginField = loginField;
+        this.passwordField = passwordField;
+    }
+
     @Override
     public void buttonClick(Button.ClickEvent event) {
-        UI.getCurrent().setContent(new MainScreen());
-        Notification.show("You've been successfully logged in");
+        if ("vaadin".equals(loginField.getValue())) {
+            UI.getCurrent().setContent(new MainScreen());
+            Notification.show("You've been successfully logged in");
+        } else {
+            Notification.show("Wrong credentials", ERROR_MESSAGE);
+        }
     }
 }
