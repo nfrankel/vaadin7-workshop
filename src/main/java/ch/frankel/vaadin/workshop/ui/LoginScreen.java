5d63172ad9388a6190352efe764359471213dbaf
Nicolas Frankel <nicolas@frankel.ch>
2014-07-27 12:09:36 +0200

    Implement a basic switch screen listener

--- src/main/java/ch/frankel/vaadin/workshop/ui/LoginScreen.java
+++ src/main/java/ch/frankel/vaadin/workshop/ui/LoginScreen.java
@@ -1,6 +1,6 @@
 package ch.frankel.vaadin.workshop.ui;
 
-import ch.frankel.vaadin.workshop.behavior.DummyClickListener;
+import ch.frankel.vaadin.workshop.behavior.LoginClickListener;
 import com.vaadin.server.Page;
 import com.vaadin.ui.*;
 
@@ -11,8 +11,8 @@ public class LoginScreen extends CustomComponent {
     public LoginScreen(AbstractOrderedLayout layout) {
         super(layout);
         layout.setMargin(true);
-        Button button = new Button("Click Me");
-        button.addClickListener(new DummyClickListener(layout));
+        Button button = new Button("Login");
+        button.addClickListener(new LoginClickListener());
         layout.addComponent(new Label("Please enter credentials"));
         TextField login = new TextField("Login:");
         login.setWidth(250, PIXELS);
