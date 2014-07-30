71671f6ca0763f204d7f77f3f804e8f6627fc309
Nicolas Frankel <nicolas@frankel.ch>
2014-07-30 22:46:13 +0200

    Migrated from in-memory container to SQL container

--- src/main/java/ch/frankel/vaadin/workshop/behavior/SendMessageClickListener.java
+++ src/main/java/ch/frankel/vaadin/workshop/behavior/SendMessageClickListener.java
@@ -1,20 +1,19 @@
 package ch.frankel.vaadin.workshop.behavior;
 
-import ch.frankel.vaadin.workshop.data.Message;
-import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.util.sqlcontainer.RowItem;
+import com.vaadin.data.util.sqlcontainer.SQLContainer;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Table;
 import com.vaadin.ui.TextArea;
 
 import java.util.Date;
 
 public class SendMessageClickListener implements Button.ClickListener {
 
-    private BeanItemContainer<Message> output;
+    private SQLContainer output;
     private TextArea input;
 
-    public SendMessageClickListener(BeanItemContainer<Message> output, TextArea input) {
+    public SendMessageClickListener(SQLContainer output, TextArea input) {
         this.output = output;
         this.input = input;
     }
@@ -24,8 +23,11 @@ public class SendMessageClickListener implements Button.ClickListener {
         String author = VaadinSession.getCurrent().getAttribute(String.class);
         String text = input.getValue();
         Date date = new Date();
-        Message message = new Message(author, text, date);
-        output.addBean(message);
+        Object rowId = output.addItem();
+        RowItem rowItem = (RowItem) output.getItem(rowId);
+        rowItem.getItemProperty("AUTHOR").setValue(author);
+        rowItem.getItemProperty("TEXT").setValue(text);
+        rowItem.getItemProperty("TIME_STAMP").setValue(date);
         input.setValue("");
     }
 }
