259ff8b7d2f2e50983dcc298d7dd8e8ab8527da6
Nicolas Frankel <nicolas@frankel.ch>
2014-08-02 21:42:18 +0200

    Implemented broadcasting to all clients

--- src/main/java/ch/frankel/vaadin/workshop/ui/MyVaadinUI.java
+++ src/main/java/ch/frankel/vaadin/workshop/ui/MyVaadinUI.java
@@ -1,6 +1,9 @@
 package ch.frankel.vaadin.workshop.ui;
 
+import ch.frankel.vaadin.workshop.push.BroadcastListener;
+import ch.frankel.vaadin.workshop.push.Broadcaster;
 import com.vaadin.annotations.PreserveOnRefresh;
+import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.FormLayout;
@@ -9,10 +12,23 @@ import com.vaadin.ui.UI;
 @SuppressWarnings("serial")
 @Title("Vaadin Workshop")
 @PreserveOnRefresh
-public class MyVaadinUI extends UI {
+@Push
+public class MyVaadinUI extends UI implements BroadcastListener<Object> {
 
     @Override
     protected void init(VaadinRequest request) {
         setContent(new LoginScreen(new FormLayout()));
+        Broadcaster.register(this);
+    }
+
+    @Override
+    public void onMessage(Object object) {
+        access(() -> setContent(new MainScreen()));
+    }
+
+    @Override
+    public void detach() {
+        Broadcaster.unregister(this);
+        super.detach();
     }
 }
